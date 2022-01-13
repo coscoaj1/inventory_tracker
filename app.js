007da1fdeacf1da08cdd4dd2e135cc29ad9b3c73
coscoaj1 <coscoanthony1@gmail.com>
2022-01-13 05:59:53 -0500

    get all route created

--- app.js
+++ app.js
@@ -10,24 +10,47 @@ app.get("test", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.post("/inventory", async (req, res) => {
-  const { product_name, sku, location, count, id } = req.body;
-  const body = req.body;
-  console.log(body);
-
-  if (!product_name || !sku || !location || !count || !id) {
-    return res.status(401).json({ error: "missing or invalid field" });
+app.get("/inventory/all", async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    console.log(products);
+    res.json(products);
+  } catch (e) {
+    console.log(e);
   }
+});
 
-  const newProduct = await Product.create({
-    product_name: body.product_name,
-    sku: body.sku,
-    location: body.location,
-    count: body.count,
-    id: body.id,
-  });
+app.post("/inventory", async (req, res) => {
+  try {
+    const { product_name, sku, location, count } = req.body;
+    const body = req.body;
+    console.log(body);
 
-  res.json(newProduct);
+    if (!product_name || !sku || !location || !count) {
+      return res.status(401).json({ error: "missing or invalid field" });
+    }
+
+    const newProduct = await Product.create({
+      product_name: body.product_name,
+      sku: body.sku,
+      location: body.location,
+      count: body.count,
+    });
+
+    res.json(newProduct);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.delete("/inventory/:id", async (req, res) => {
+  try {
+    await Product.destroy({
+      where: { id: req.params.id },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 morgan.token("body", (req) => JSON.stringify(req.body));
