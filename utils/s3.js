6c05313374cb9cd605ad67acaafdea7d98ce05a3
coscoaj1 <coscoanthony1@gmail.com>
2022-01-17 15:19:00 -0500

    updated .env

--- utils/s3.js
+++ utils/s3.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 const S3 = require("aws-sdk/clients/s3");
 
 const region = config.REGION;
-const accessKeyId = config.AWS_ACCESS_KEY_ID;
-const secretAccessKey = config.AWS_SECRET_ACCESS_KEY;
-const bucketName = config.AWS_BUCKET_NAME;
+const accessKeyId = config.ACCESS_KEY_ID;
+const secretAccessKey = config.SECRET_ACCESS_KEY;
+const bucketName = config.BUCKET_NAME;
 
 const s3 = new S3({
   region,
