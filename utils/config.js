285d862b279190588397b555475488fa6e49c999
coscoaj1 <coscoanthony1@gmail.com>
2022-01-17 16:47:46 -0500

    updated config vars

--- utils/config.js
+++ utils/config.js
@@ -5,16 +5,16 @@ const MYSQL_URI = process.env.MYSQL_URI;
 const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 const DATABASE = process.env.DATABASE;
-const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
-const REGION = process.env.AWS_BUCKET_REGION;
-const AWS_ACCESS_KEY_ID = process.env_AWS_ACCESS_KEY_ID;
-const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const REGION = process.env.BUCKET_REGION;
+const ACCESS_KEY_ID = process.envACCESS_KEY_ID;
+const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
 
 module.exports = {
-  AWS_BUCKET_NAME,
+  BUCKET_NAME,
   REGION,
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
+  ACCESS_KEY_ID,
+  SECRET_ACCESS_KEY,
   MYSQL_URI,
   PORT,
   USER,
