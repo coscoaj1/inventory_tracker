de8e1598b689c4e51832d0c7337387261e4c5f9f
Anthony Cosco <coscoanthony1@gmail.com>
2023-03-11 08:49:05 -0500

    removed dbstart file and updated db.js

--- utils/db.js
+++ utils/db.js
@@ -1,24 +1,14 @@
-// const { Client } = require("pg");
-
-
-// const client = new Client();
-
-// client
-//   .connect()
-//   .then(() => console.log("connected"))
-//   .catch((err) => console.error("connection error", err.stack));
-
 const { Client } = require("pg");
-require("dotenv").config();
+const config = require("./config");
 
 
 module.exports.getClient = async () => {
   const client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+    host: config.PG_HOST,
+    port: config.PG_PORT,
+    user: config.PG_USER,
+    password: config.PG_PASSWORD,
+    database: config.PG_DATABASE,
   });
   // await client.connect();
   return client;
