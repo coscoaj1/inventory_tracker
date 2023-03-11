e3f6a03c98effa83ac833774aafe76b15fee8060
Anthony Cosco <coscoanthony1@gmail.com>
2023-03-11 08:48:42 -0500

    remove dbstart

--- utils/db_old.js
+++ utils/db_old.js
@@ -1,5 +1,5 @@
 // const Sequelize = require("sequelize");
-// const config = require("./config");
+const config = require("./config");
 // console.log("user:", config.USER);
 
 const { RDS } = require("aws-sdk")
