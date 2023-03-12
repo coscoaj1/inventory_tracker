63085cbcf27d3668c6567e4dbf2d4a3f8a031d08
Anthony Cosco <coscoanthony1@gmail.com>
2023-03-12 06:18:15 -0400

    updated routes

--- utils/db.js
+++ utils/db.js
@@ -1,26 +1,12 @@
-const { Client } = require("pg");
 const config = require("./config");
-
-
-module.exports.getClient = async () => {
-  const client = new Client({
-    host: config.PG_HOST,
-    port: config.PG_PORT,
-    user: config.PG_USER,
-    password: config.PG_PASSWORD,
-    database: config.PG_DATABASE,
-  });
-  // await client.connect();
-  return client;
-};
-const { Pool } = require('pg')
+const { Pool } = require("pg");
  
 const pool = new Pool({host: config.PG_HOST,
   port: config.PG_PORT,
   user: config.PG_USER,
   password: config.PG_PASSWORD,
-  database: config.PG_DATABASE,})
+  database: config.PG_DATABASE,});
  
 module.exports = {
   query: (text, params) => pool.query(text, params),
-}
+};
